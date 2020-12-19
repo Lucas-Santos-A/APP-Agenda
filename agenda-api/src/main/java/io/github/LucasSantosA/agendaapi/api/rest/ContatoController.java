cd19a05703cf9563a492ce3df799bf100c64b278
Lucas-Santos-A <lucas.salmeida@hotmail.com>
2020-12-19 20:17:18 -0300

    Adicao de cors

--- agenda-api/src/main/java/io/github/LucasSantosA/agendaapi/api/rest/ContatoController.java
+++ agenda-api/src/main/java/io/github/LucasSantosA/agendaapi/api/rest/ContatoController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/contatos")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ContatoController {
 
     //propriedades com final são obrigatórias para a inicialização da classe
