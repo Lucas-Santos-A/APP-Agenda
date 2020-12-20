0c78c78c9334b3927c6fec27d2ac8a2b65754093
Lucas-Santos-A <lucas.salmeida@hotmail.com>
2020-12-20 17:33:53 -0300

    Upload de imagens

--- agenda-app/src/app/contato/contato.component.ts
+++ agenda-app/src/app/contato/contato.component.ts
@@ -13,7 +13,7 @@ export class ContatoComponent implements OnInit {
 
   formulario: FormGroup;
   contatos: Contato[] = [];
-  colunas = ['id', 'nome', 'email', 'favorito'];
+  colunas = ['foto', 'id', 'nome', 'email', 'favorito'];
 
   constructor(
     private service: ContatoService,
@@ -54,6 +54,17 @@ export class ContatoComponent implements OnInit {
       let lista: Contato[] = [...this.contatos, response];
       this.contatos = lista;
     })
+  }
+
+  uploadFoto(event: Event, contato: Contato){
+    const files = (<HTMLInputElement>event.target).files;
+    if(files){
+      const foto = files[0];
+      const formData: FormData = new FormData();
+      formData.append("foto", foto);
+
+      this.service.upload(contato, formData).subscribe(response => this.listarContatos())
+    }
   }
 
 }
