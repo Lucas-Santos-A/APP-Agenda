302b82f3135fd738b477ce7518bb2d3695f113cc
Lucas-Santos-A <lucas.salmeida@hotmail.com>
2020-12-20 15:28:07 -0300

    Feature favoritar contato

--- agenda-app/src/app/contato.service.ts
+++ agenda-app/src/app/contato.service.ts
@@ -26,4 +26,8 @@ export class ContatoService {
     return this.http.get<any>(this.url);
   }
 
+  favorite(contato: Contato) : Observable<any> {
+    return this.http.patch(`${this.url}/${contato.id}/favorito`, null);
+  }
+
 }
